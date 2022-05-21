e9c23514d248e31343550dc7855b736ab5c6a272
Brady Mavetz <b.mavetz@outlook.com>
2022-05-20 20:59:45 -0500

    modified webpack file and installed package.json on server route

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
+const WorkboxPlugin = require("workbox-webpack-plugin");
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
@@ -33,7 +33,7 @@ module.exports = () => {
         inject: true,
         name: 'Text Editor',
         short_name: 'Editor',
-        description: 'Keep a running list of notes.',
+        description: 'PWA for a text editor.',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
@@ -45,6 +45,25 @@ module.exports = () => {
             destination: path.join('assets', 'icons'),
           },
         ],
+      }),
+      new WorkboxPlugin.GenerateSW({
+        exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+  
+        runtimeCaching: [{
+          
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+  
+          handler: 'CacheFirst',
+  
+          options: {
+          
+            cacheName: 'images',
+  
+            expiration: {
+              maxEntries: 2,
+            },
+          },
+        }],
       }),
     ],
 
